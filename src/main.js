7cbb41603cabff3ce22a0ae0e6d45fe84716c5fb
Ula Latush <ula2sh@gmail.com>
2020-09-03 17:36:54 +0300

    second commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+
+import {createSiteMenuTemplate} from "./view/site-menu.js";
+import {createFilterTemplate} from "./view/filter.js";
+import {createBoardTemplate} from "./view/board.js";
+import {createTaskTemplate} from "./view/task.js";
+import {createTaskEditTemplate} from "./view/task-editor.js";
+import {createLoadMoreButtonTemplate} from "./view/button.js";
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
+
+
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
+render(siteMainElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, createBoardTemplate(), `beforeend`);
+
+const boardElement = siteMainElement.querySelector(`.board`);
+const taskListElement = boardElement.querySelector(`.board__tasks`);
+
+render(taskListElement, createTaskEditTemplate(), `beforeend`);
+
+render(taskListElement, createTaskTemplate(), `beforeend`);
+render(taskListElement, createTaskTemplate(), `beforeend`);
+render(taskListElement, createTaskTemplate(), `beforeend`);
+
+render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
